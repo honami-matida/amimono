bf97d529c7804adcca50dc6cfaecb401000d7981
honami-matida <yuzuyuzu202405@gmail.com>
2024-06-09 11:41:23 +0000

    [finish]削除機能

---
+++ app/controllers/works_controller.rb
@@ -0,0 +1,46 @@
+class WorksController < ApplicationController
+
+  def new
+    @work = Work.new
+  end
+
+  def create
+    @work = Work.new(work_params)
+    if @work.save
+      redirect_to work_path(work.id)
+    else
+      render :new
+    end
+  end
+
+  def index
+    @works = Work.all
+  end
+
+  def show
+    @work = Work.find(params[:id])
+  end
+
+  def edit
+    @work = Work.find(params[:id])
+  end
+
+  def update
+    work = Work.find(params[:id])
+    work.update(work_params)
+    redirect_to work_path(work.id)
+  end
+
+  def destroy
+    work = Work.find(params[:id])
+    work.destroy
+    redirect_to works_path
+  end
+
+  private
+
+  def work_params
+    params.require(:work).permit(:title, :body, :image)
+  end
+
+end
---
+++ app/models/work.rb
@@ -0,0 +1,7 @@
+class Work < ApplicationRecord
+  has_one_attached :image
+
+  validates :title, presence: true
+  validates :body, presence: true
+  validates :image, presence: true
+end
